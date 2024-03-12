4a7a19374500a8c3071d4e991f65827880d7c1a5
iamvarshinigowda <108373070+iamvarshinigowda@users.noreply.github.com>
2024-03-12 09:19:10 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import psycopg2
+
+
+
+# Define database connection parameters
+DB_NAME = 'Varshini'
+DB_USER = 'Varshini'
+DB_PASSWORD = 'Man_4569'
+DB_HOST = 'local_pgdb'
+DB_PORT = '5432'
+
+# Define a function to connect to the PostgreSQL database
+def connect_to_db():
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    return conn
+
+
+
+    conn.close()
+
+    return 'Data submitted successfully'
+
